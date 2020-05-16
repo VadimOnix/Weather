70dc82c4205a82ffd47231a28bca709703e50255
Vadim Korolev <vkorolev1506@gmail.com>
2020-05-16 15:47:31 +0300

    dev: add Five Days Responce type

---
+++ WeatherWinFormsApp/WeatherWinFormsApp/API/FiveDaysWeatherResponceType.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherWinFormsApp.API
+{
+    class WeatherListData
+    {
+        public int dt;
+        public Main Main;
+        public List<WeatherItem> weather = new List<WeatherItem>();
+        public Clouds clouds;
+        public Wind wind;
+        public Dictionary<string,float> rain = new Dictionary<string, float> { { "3h", 0 } };
+        public Dictionary<string,string> sys = new Dictionary<string, string> { { "pod", "d" } };
+        public string dt_txt;
+    }
+    class FiveDaysWeatherResponceType
+    {
+        public int cod;
+        public int message;
+        public int cnt;
+        public List<WeatherListData> list = new List<WeatherListData>();
+        public City city;
+    }
+}
