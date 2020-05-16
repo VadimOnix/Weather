e37361e111c602c3c51f64d0c98f2e51da06f89a
Vadim Korolev <vkorolev1506@gmail.com>
2020-05-16 14:47:55 +0300

    refactor: move typed classes to CommonTypes.cs

---
+++ WeatherWinFormsApp/WeatherWinFormsApp/API/CommonTypes.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherWinFormsApp.API
+{
+
+    public class Coord
+    {
+        float lon;
+        float lat;
+    }
+
+    class WeatherItem
+    {
+        public int id;
+        public string main;
+        public string description;
+        public string icon;
+    }
+
+    class Main
+    {
+        public float temp;
+        public float feels_like;
+        public int temp_min;
+        public float temp_max;
+        public int pressure;
+        public int humidity;
+    }
+    class Wind
+    {
+        public float speed;
+        public int deg;
+    }
+    class Clouds
+    {
+        public int all;
+    }
+    class Sys
+    {
+        public int type;
+        public int id;
+        public string country;
+        public int sunrise;
+        public int sunset;
+    }
+
+}
--- WeatherWinFormsApp/WeatherWinFormsApp/API/CurrentWeatherResponceType.cs
+++ WeatherWinFormsApp/WeatherWinFormsApp/API/CurrentWeatherResponceType.cs
@@ -6,46 +6,6 @@ using System.Threading.Tasks;
 
 namespace WeatherWinFormsApp.API
 {
-    class Coord
-    {
-        public float lon;
-        public float lat;
-    }
-
-    class WeatherItem
-    {
-        public int id;
-        public string main;
-        public string description;
-        public string icon;
-    }
-
-    class Main
-    {
-        public float temp;
-        public float feels_like;
-        public int temp_min;
-        public float temp_max;
-        public int pressure;
-        public int humidity;
-    }
-    class Wind
-    {
-        public float speed;
-        public int deg;
-    }
-    class Clouds
-    {
-        public int all;
-    }
-    class Sys
-    {
-        public int type;
-        public int id;
-        public string country;
-        public int sunrise;
-        public int sunset;
-    }
     class CurrentWeatherResponceType
     {
         public Coord coord;
