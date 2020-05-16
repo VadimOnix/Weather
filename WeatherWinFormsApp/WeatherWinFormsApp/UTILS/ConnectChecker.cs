4be8a637da45a780706dd5797bd53d984a0b5fea
Vadim Korolev <vkorolev1506@gmail.com>
2020-05-16 16:21:53 +0300

    dev: add Internet Connection Checker

---
+++ WeatherWinFormsApp/WeatherWinFormsApp/UTILS/ConnectChecker.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherWinFormsApp.UTILS
+{
+    /// <summary>
+    /// Класс, проверяющий интернет соединение
+    /// </summary>
+    class ConnectChecker
+    {
+        public bool IsAvailable()
+        {
+            try
+            {
+                var client = new WebClient();
+                using (client.OpenRead("https://www.google.com"))
+                {
+                    return true;
+                }
+            } catch
+            {
+                return false;
+            }
+        }
+    }
+}
