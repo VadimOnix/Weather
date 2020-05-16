3d04192a8dff4e7a2e5c40fe16b1fabe6364ee6d
Vadim Korolev <vkorolev1506@gmail.com>
2020-05-16 15:46:15 +0300

    fix: changed simple types

--- WeatherWinFormsApp/WeatherWinFormsApp/API/CommonTypes.cs
+++ WeatherWinFormsApp/WeatherWinFormsApp/API/CommonTypes.cs
@@ -28,7 +28,7 @@ namespace WeatherWinFormsApp.API
     {
         public float temp;
         public float feels_like;
-        public int temp_min;
+        public float temp_min;
         public float temp_max;
         public int pressure;
         public int humidity;
